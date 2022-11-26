2fd0494991a2ddec6268934b9ef1248fbc3ada13
ozamar1212 <ozamar1212@gmail.com>
2022-11-26 14:07:15 +0200

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-crm-fac1b',
+    appId: '1:427245576931:web:ee31e8ca9f3346e2a31bd6',
+    storageBucket: 'angular-crm-fac1b.appspot.com',
+    apiKey: 'AIzaSyAn9OWLcTc5dn9w2xF_1HCXD8fE10_zfMY',
+    authDomain: 'angular-crm-fac1b.firebaseapp.com',
+    messagingSenderId: '427245576931',
+  },
   production: false
 };
 
